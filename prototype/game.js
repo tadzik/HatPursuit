173c076218012f622636fc0a105324bc8202844d
Tadeusz Sośnierz <tadeusz.sosnierz@onet.pl>
2015-06-26 21:34:09 +0200

    More cars, and randomized

--- prototype/game.js
+++ prototype/game.js
@@ -6,20 +6,26 @@ function init() {
 
 function update() {
     var newcars = []
+    var roomForMore = true
     for (var i = 0; i < cars.length; i++) {
         var car = cars[i]
         car.y += car.velocity
         if (car.y <= screen.height) {
             newcars.push(car)
+            if (car.y < 0) {
+                roomForMore = false
+            }
         } else {
             car.destroy()
         }
     }
+
     cars = newcars
 
-    while (cars.length < 1) {
+    if (roomForMore) {
         var c = carComponent.createObject(screen, { x: 200, y: 0 })
-        c.y -= c.height
+        c.y -= c.height * 1.5 // half a car of space between cars
+        c.x = Math.random() * (screen.width - c.width)
         c.velocity = 10
         if (c == null) {
             console.log("ERROR: " + carComponent.errorString())
